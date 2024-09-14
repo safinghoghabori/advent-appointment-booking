4adde14e6576e1465ff1eaf5d181178e0bbaaff6
safinghoghabori <safinghoghabori65@gmail.com>
2024-09-14 10:01:35 +0530

    add models

---
+++ Models/Appointment.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace advent_appointment_booking.Models
+{
+    public class Appointment
+    {
+        [Key]  
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AppointmentId { get; set; }
+
+        [ForeignKey("TruckingCompany")]  
+        public int TrCompanyId { get; set; }
+
+        [ForeignKey("Terminal")]  
+        public int TerminalId { get; set; }
+
+        [ForeignKey("Driver")]
+        public int DriverId { get; set; }
+
+        public string MoveType { get; set; }
+        public string ContainerNumber { get; set; }
+        public string SizeType { get; set; }
+        public string Line { get; set; }
+        public string ChassisNo { get; set; }
+        public string AppointmentStatus { get; set; }
+        public DateTime AppointmentCreated { get; set; }
+        public DateTime AppointmentValidThrough { get; set; }
+        public DateTime AppointmentLastModified { get; set; }
+        public string GateCode { get; set; }
+
+        public TruckingCompany TruckingCompany { get; set; }
+        public Terminal Terminal { get; set; }
+        public Driver Driver { get; set; }
+    }
+}
